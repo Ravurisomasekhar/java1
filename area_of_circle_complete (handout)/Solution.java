d6c3311642544cd4d6bb7d929a27dc7f76d3345f
Ravurisomasekhar <ravurisomasekhar@msitprogram.net>
2025-03-17 15:58:53 +0530

    Add files via upload

---
+++ area_of_circle_complete (handout)/Solution.java
@@ -0,0 +1,14 @@
+// # Write your solution here
+
+
+import java.util.Scanner;
+
+public class Solution{  
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);  
+        float r = sc.nextFloat();
+        double Area;
+        Area = Math.PI*Math.pow(r,2);        
+     System.out.printf("%.2f",Area);  
+    }  
+}  
