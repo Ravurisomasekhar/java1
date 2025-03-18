c8d63578d72f2b055fb4ba7d983e247554f3cf12
Ravurisomasekhar <ravurisomasekhar@msitprogram.net>
2025-03-18 15:32:30 +0530

    Add files via upload

---
+++ UseThree - student/UseThree - student/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+
+     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        String s3 = sc.next();
+        System.out.println("Hi "+ s3 + ", " + s2 + ", and " + s1 + ".");
+    }
+}
